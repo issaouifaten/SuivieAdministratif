73787ffb29498052c811d813b43eb2dd66d98b5d
issaouifaten <issaouifaten10@gmail.com>
2020-12-22 09:08:35 +0100

    commit FATEN 22/12 activity

---
+++ app/src/main/java/com/example/suivieadministratif/ui/statistique_rapport_activite/Client/SuivieCommandeNonConformeClient.java
@@ -0,0 +1,16 @@
+package com.example.suivieadministratif.ui.statistique_rapport_activite.Client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.suivieadministratif.R;
+
+public class SuivieCommandeNonConformeClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suivie_commande_non_conforme_client);
+    }
+}
---
+++ app/src/main/java/com/example/suivieadministratif/ui/statistique_rapport_activite/Fournisseur/CommandeFournisseurNonConforme.java
@@ -0,0 +1,16 @@
+package com.example.suivieadministratif.ui.statistique_rapport_activite.Fournisseur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.suivieadministratif.R;
+
+public class CommandeFournisseurNonConforme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commande_fournisseur_non_conforme);
+    }
+}
